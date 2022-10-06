70fbc97e3093db225d4f0409308b988cee5d4222
Марат Хайруллин <hottabich1337@mail.ru>
2022-10-06 23:31:01 +0300

    Added a message recording function

--- src/main/java/com/example/marik/domain/Role.java
+++ src/main/java/com/example/marik/domain/Role.java
@@ -1,5 +1,13 @@
 package com.example.marik.domain;
 
-public enum Role {
+import org.springframework.security.core.GrantedAuthority;
+
+public enum Role implements GrantedAuthority {
+
     USER;
+
+    @Override
+    public String getAuthority() {
+        return name();
+    }
 }
